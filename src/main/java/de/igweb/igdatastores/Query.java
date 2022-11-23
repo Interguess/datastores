91b79f6df732beff2c22cbf14ef2c230a6001421
JonasDevDE <73913242+JonasDevDE@users.noreply.github.com>
2022-11-23 17:36:35 +0100

    Moved Field.java to Query.Field

--- src/main/java/de/igweb/igdatastores/Query.java
+++ src/main/java/de/igweb/igdatastores/Query.java
@@ -1,5 +1,7 @@
 package de.igweb.igdatastores;
 
+import lombok.Getter;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -38,6 +40,120 @@ public class Query<T> {
 
     public List<T> get() {
         return dataStore.get(this);
+    }
+
+    @Getter
+    @SuppressWarnings("unused")
+    public static class Field {
+
+        private final Query<?> query;
+
+        private final String name;
+
+        private String condition;
+
+        private Object requiredValue;
+
+        public Field(Query<?> query, String name) {
+            this.query = query;
+            this.name = name;
+        }
+
+        public Query<?> equal(Object requiredValue) {
+            this.condition = "=";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> equalIgnoreCase(Object requiredValue) {
+            this.condition = "=?^";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> notEqual(Object requiredValue) {
+            this.condition = "!=";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> greaterThan(Object requiredValue) {
+            this.condition = ">";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> lessThan(Object requiredValue) {
+            this.condition = "<";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> contains(Object requiredValue) {
+            this.condition = "contains";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> containsIgnoreCase(Object requiredValue) {
+            this.condition = "containsIgnoreCase";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> startsWith(Object requiredValue) {
+            this.condition = "startsWith";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> startsWithIgnoreCase(Object requiredValue) {
+            this.condition = "startsWithIgnoreCase";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> endsWith(Object requiredValue) {
+            this.condition = "endsWith";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> endsWithIgnoreCase(Object requiredValue) {
+            this.condition = "endsWithIgnoreCase";
+            this.requiredValue = requiredValue;
+            return pack();
+        }
+
+        public Query<?> isNull() {
+            this.condition = "?null";
+            this.requiredValue = null;
+            return pack();
+        }
+
+        public Query<?> isNotNull() {
+            this.condition = "?!null";
+            this.requiredValue = null;
+            return pack();
+        }
+
+        public Query<?> isTrue() {
+            this.condition = "?true";
+            this.requiredValue = null;
+            return pack();
+        }
+
+        public Query<?> isFalse() {
+            this.condition = "?false";
+            this.requiredValue = null;
+            return pack();
+        }
+
+        public Query<?> pack() {
+            this.query.addField(this);
+            return this.query;
+        }
+
     }
 
 }
